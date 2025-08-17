f384088950b2423ad0ad5cbb643c213aecb21c5c
Fair <541077827@qq.com>
2025-08-18 01:48:49 +0800

    update

--- utility.py
+++ utility.py
@@ -61,8 +61,6 @@ DEFAULT_SCRIPT = "RESULT = (A, B, C, D)"
 
 
 class AnyType(str):
-    """A special class that is always equal in not equal comparisons. Credit to pythongosssss"""
-
     def __ne__(self, __value: object) -> bool:
         return False
 
